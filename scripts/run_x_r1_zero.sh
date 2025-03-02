426cc3727c6aff97057d0da3b81563699bc8e5fc
CHEN Zizhe <zzchen2@linux15.cse.cuhk.edu.hk>
2025-03-02 13:01:12 +0800

    Init grpo plus

--- scripts/run_x_r1_zero.sh
+++ scripts/run_x_r1_zero.sh
@@ -49,5 +49,19 @@ ACCELERATE_LOG_LEVEL=info accelerate launch \
 ACCELERATE_LOG_LEVEL=info accelerate launch \
 --config_file recipes/zero3.yaml \
 --num_processes=3 src/x_r1/grpo.py \
---config recipes/X_R1_zero_3B_config_low_memory.yaml \
+--config recipes/X_R1_zero_3B_config.yaml \
 > ./output/x_r1_3B_sampling.log 2>&1
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./src/x_r1/benchmark.py \
+	--model_name='record/X-R1-3B_0' \
+    --dataset_name='HuggingFaceH4/MATH-500' \
+	--output_name='./output/result_benchmark_math500'  \
+	--max_output_tokens=1024 \
+	--num_gpus=4
+
+CUDA_VISIBLE_DEVICES=0 python ./src/x_r1/benchmark.py \
+	--model_name='record/X-R1-3B_0' \
+    --dataset_name='HuggingFaceH4/MATH-500' \
+	--output_name='./output/result_benchmark_math500'  \
+	--max_output_tokens=1024 \
+	--num_gpus=1

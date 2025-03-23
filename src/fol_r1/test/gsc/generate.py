c6f75dce86934756bb2ef0cf4ebd038020127561
VOXXXX1874 <1155173938@link.cuhk.edu.hk>
2025-03-23 10:05:08 +0800

    naive gsc dataset

---
+++ src/fol_r1/test/gsc/generate.py
@@ -0,0 +1,82 @@
+import random
+import argparse
+import json
+
+def parse_args():
+    """
+    Parse command-line arguments.
+    """
+    parser = argparse.ArgumentParser()
+
+    # Define the command-line arguments
+    parser.add_argument("--num_of_var", type=int, default=8, help="Number of variables")
+    parser.add_argument("--num_of_generation", type=int, default=3, help="Number of generations")
+    parser.add_argument("--range", type=int, default=2, help="Range of numbers")
+    parser.add_argument("--num_of_questions", type=int, default=7500, help="Number of questions")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    dataset = []
+
+    for _ in range(args.num_of_questions):
+        num_of_var = args.num_of_var + random.randint(-args.range, args.range)
+        num_of_generation = (args.num_of_generation + + random.randint(-args.range, args.range)) * num_of_var
+
+        # Randomly choose number of variables from a to z
+        variables = random.sample(range(97, 123), num_of_var)
+        variables = [chr(i) for i in variables]
+
+        # initialize the value of variables by random number
+        values = [random.randint(-2, 2) for _ in range(num_of_var)]
+        values = dict(zip(variables, values))
+
+        prompt = '''Given the following variables and operations, what is the final value of each variable:\n'''
+        for var, val in values.items():
+            prompt += f"{var} = {val}\n"
+
+        equations = []
+        # loop through the number of generations
+        for _ in range(num_of_generation):
+            # randomly choose the variable
+            var_1 = random.choice(variables)
+            var_2 = random.choice(variables)
+            var_3 = random.choice(variables)
+            # randomly choose + or - or * or /
+            if values[var_3] != 0:
+                op = random.choice(["+", "-", "*", "/"])
+            else:
+                op = random.choice(["+", "-", "*"])
+            # generate the equation
+            equation = f"{var_1} = {var_2} {op} {var_3}"
+            equations.append(equation)
+            # Update the value of the variable
+            if op == "+":
+                values[var_1] = values[var_2] + values[var_3]
+            elif op == "-":
+                values[var_1] = values[var_2] - values[var_3]
+            elif op == "*":
+                values[var_1] = values[var_2] * values[var_3]
+            elif op == "/":
+                values[var_1] = values[var_2] / values[var_3]
+
+        # Emsemble the prompt
+        for _, equation in enumerate(equations):
+            prompt += f"{equation}\n"
+
+        # Emsemble the solution
+        solution = ""
+        for var, val in values.items():
+            solution += f"{var} = {val}, "
+
+        dataset.append({
+            "prompt": prompt,
+            "solution": solution
+        })
+
+    # Save the dataset
+    with open("src/fol_r1/test/gsc/GSC.json", "w") as f:
+        json.dump(dataset, f, indent=4)
+
+

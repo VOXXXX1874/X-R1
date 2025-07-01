b9c6cf0a3f112dbdbe6e9810c7b61c6cdf392e26
CHEN Zizhe <zzchen2@linux15.cse.cuhk.edu.hk>
2025-07-01 09:00:55 +0800

    tuning hyperparameter

--- slurm.sh
+++ slurm.sh
@@ -3,16 +3,15 @@
 #SBATCH --mail-user=zzchen2@cse.cuhk.edu.hk
 #SBATCH --mail-type=ALL
 #SBATCH --output=/research/d2/spc/zzchen2/X-R1/tmp/results.txt
-#SBATCH --gres=gpu:7
-#SBATCH --qos=gpu
-#SBATCH -p gpu_24h
+#SBATCH --gres=gpu:5
+#SBATCH --reser=jcheng_gpu_301
 #SBATCH -c 24
-#SBATCH -w projgpu13
+#SBATCH -p gpu_24h
 
 ACCELERATE_LOG_LEVEL=info \
 accelerate launch \
 --config_file recipes/zero3.yaml \
---num_processes=6 \
+--num_processes=4 \
 src/x_r1/grpo.py \
 --config recipes/X_R1_1dot5B_config_math_num.yaml \
 > ./output/X_R1_1dot5B_math_num.log 2>&1

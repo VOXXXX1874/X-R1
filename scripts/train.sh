42671054fa5c4bee054f6bc848bacedde7d22d11
VOXXXX1874 <1155173938@link.cuhk.edu.hk>
2025-08-15 10:19:51 +0800

    Some interesting parameter tuning

--- scripts/train.sh
+++ scripts/train.sh
@@ -73,6 +73,14 @@ src/x_r1/grpo.py \
 --config recipes/X_R1_1dot5B_config_math_num.yaml \
 > ./output/X_R1_1dot5B_math_num.log 2>&1
 
+ACCELERATE_LOG_LEVEL=info \
+accelerate launch \
+--config_file recipes/zero3.yaml \
+--num_processes=4 \
+src/x_r1/grpo.py \
+--config recipes/X_R1_1dot5B_config_math_num.yaml \
+> ./output/X_R1_1dot5B_math_num.log 2>&1
+
 # MDP
 
 ACCELERATE_LOG_LEVEL=info \
